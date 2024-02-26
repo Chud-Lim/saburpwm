b89d2df9f96676f961924c7c386a7ce2979b0f86
Chud <limrobert.wo@gmail.com>
2024-02-26 19:39:04 +0800

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+console.log('Working JS');
+
+// var stripe = Stripe('pk_test_51ONUcTC1JCiZ2EZ1uW83zu8UzEYDCUl8kTJN2JhgRdtlpJ7mpiycQiNQwgVg2Q6orjkfBncGPf5Tl6iUcGGnJ3El006FWbU0CT');
+
+// // var elements = stripe.elements({
+// //   clientSecret: 'sk_test_51ONUcTC1JCiZ2EZ1U27AFtni8YVEUU8AXTsRZH8cdoAu1US1EHrsVmq9tA5RJqhyPXgTmRPJPNEdKzt7twWa1VkH00m5hTY2ja'
+// // });
+
+// var elements = stripe.elements({
+//   mode: 'payment',
+//   currency: 'usd',
+//   amount: 1099,
+// });
+
+// var paymentElement = elements.create('payment'); 
